344cca93f0138547401564acf86d39fc1c9becd3
Ayaan <ayaan.lone12@gmail.com>
2020-10-26 17:37:15 +0530

    I am adding this File
    Signed-off-by: Ayaan <ayaan.lone12@gmail.com>

---
+++ new.js
@@ -0,0 +1,14 @@
+// // int char float etc 
+// // VAR-> All kind of variables can be stored
+// var age,days;
+// age=18;
+// days=365*age;
+// var name;  
+// name= "Aashi Gupta"; //char name; // name = 'Aashi'
+// document.write("Hello From Javascript")
+// console.log("Hello From Console Log")
+// document.getElementById("second").innerHTML = 10+2;
+
+
+// alert("Hello "+name+" Your age is: "+days)
+// document.getElementById("header").innerHTML=10+2; 
