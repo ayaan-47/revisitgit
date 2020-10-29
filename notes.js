dec9a84c370c6365921bc33c2c8fc11d0f8de076
ayaan-47 <68777461+ayaan-47@users.noreply.github.com>
2020-10-29 23:32:10 +0530

    Updated again

--- notes.js
+++ notes.js
@@ -326,3 +326,34 @@ Example
  </body>
 
  </html>
+
+
+Common HTML Events
+Here is a list of some common HTML events:
+
+Event	Description
+onchange	An HTML element has been changed
+onclick	The user clicks an HTML element
+onmouseover	The user moves the mouse over an HTML element
+onmouseout	The user moves the mouse away from an HTML element
+onkeydown	The user pushes a keyboard key
+onload	The browser has finished loading the page
+
+Link for More events https://www.w3schools.com/jsref/dom_obj_event.asp
+
+
+
+######################################################
+
+Strings have started 
+
+Six other escape sequences are valid in JavaScript:
+
+Code	Result
+\b	Backspace
+\f	Form Feed
+\n	New Line
+\r	Carriage Return
+\t	Horizontal Tabulator
+\v	Vertical Tabulator
+THESE ARE THE ESCAPE CHARS
